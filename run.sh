f049cdd2640660861720121da16c79ad6b3525b0
Alexey Kuznetsov <alexey.k.kuznetsov@gsk.com>
2022-10-24 09:14:12 -0700

    Pushed from Domino: https://domino-dev.gsk.com/workspace/ak956494/Holoviz_testing?executionId=6356b895101cf707d9296570

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-venv/bin/python -m panel serve app.py --show --autoreload
+python -m pip install -r requirements.txt --user
+python -m panel serve app.py --address="0.0.0.0" --port=8888 --autoreload
