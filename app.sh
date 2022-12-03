c8da77393a969420fa41f89456a59562e20f2d34
Alexey Kuznetsov <42695162+paltusplintus@users.noreply.github.com>
2022-12-03 17:04:50 +0100

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,5 +1,2 @@
-#BOKEH_ALLOW_WS_ORIGIN=run-$DOMINO_RUN_ID.domino-compute.svc.cluster.local:80
-BOKEH_ALLOW_WS_ORIGIN=*
-echo DOMINO_RUN_ID=$DOMINO_RUN_ID
 python -m pip install -r requirements.txt --user
-python -m panel serve app.py --address="0.0.0.0" --port=8888 --autoreload
+python -m panel serve app.py --address="0.0.0.0" --port=8888 --autoreload --allow-websocket-origin=*
