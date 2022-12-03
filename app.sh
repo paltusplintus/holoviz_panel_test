1b6e6678b7f2e0f17b84ace83c5c14527f182d3c
Alexey Kuznetsov <42695162+paltusplintus@users.noreply.github.com>
2022-12-03 17:01:51 +0100

    Update app.sh

--- app.sh
+++ app.sh
@@ -2,4 +2,4 @@
 BOKEH_ALLOW_WS_ORIGIN=*
 echo DOMINO_RUN_ID=$DOMINO_RUN_ID
 python -m pip install -r requirements.txt --user
-python -m panel serve app.py --address="localhost" --port=8888 --autoreload
+python -m panel serve app.py --address="0.0.0.0" --port=8888 --autoreload
