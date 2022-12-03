28f7aeefe0de989f3ba8605529882e12791da49c
Alexey Kuznetsov <42695162+paltusplintus@users.noreply.github.com>
2022-12-03 17:01:07 +0100

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,4 +1,5 @@
-BOKEH_ALLOW_WS_ORIGIN=run-$DOMINO_RUN_ID.domino-compute.svc.cluster.local:80
+#BOKEH_ALLOW_WS_ORIGIN=run-$DOMINO_RUN_ID.domino-compute.svc.cluster.local:80
+BOKEH_ALLOW_WS_ORIGIN=*
 echo DOMINO_RUN_ID=$DOMINO_RUN_ID
 python -m pip install -r requirements.txt --user
 python -m panel serve app.py --address="localhost" --port=8888 --autoreload
