8bc323d8f0095634bd2461ee5606ad79cacfa808
Alexey Kuznetsov <paltusplintus@gmail.com>
2022-12-03 16:45:05 +0100

    added BOKEH_ALLOW_WS_ORIGIN in app.sh

--- app.sh
+++ app.sh
@@ -1,2 +1,3 @@
+BOKEH_ALLOW_WS_ORIGIN=run-$DOMINO_RUN_ID.domino-compute.svc.cluster.local:80
 python -m pip install -r requirements.txt --user
 python -m panel serve app.py --address="0.0.0.0" --port=8888 --autoreload
